e2042c87d61a96ecbc63477f640c4c38ca35dd1f
Carlos Alejandro Hernandez Mejia <alejandromejia2013.27@outlook.com>
2020-04-03 17:40:44 -0300

    Autenticación

---
+++ apps/core/authentication.py
@@ -0,0 +1,24 @@
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data,
+            context={
+                'request': request
+            }
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.valida_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user': {
+                'id': user.id,
+                'username': user.username,
+                'email': user.email
+            }
+        })
